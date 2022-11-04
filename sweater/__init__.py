3f8a88f5ae36ab6878cc40846b20b409bd6bc5f2
Slawa163 <tvm91@icloud.com>
2022-11-04 20:28:31 +0400

    0.1.3.2.2 version - добавил docker-compose.yaml & Dockerfile & .env-postgresql

--- sweater/__init__.py
+++ sweater/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
+from sweater import models, routes
+
+
 app = Flask(__name__)
 auth = Blueprint('auth', __name__)
 app.secret_key = '12QwrT!'
@@ -12,7 +15,5 @@ db = SQLAlchemy(app)
 manager = LoginManager(app)
 manager.login_view = 'auth.login'
 bootstrap = Bootstrap(app)
-
-from sweater import models, routes
 
 db.create_all()
