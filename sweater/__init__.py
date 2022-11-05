46696e6a74f08e3e111eec574db20c81835d3370
Slawa163 <tvm91@icloud.com>
2022-11-05 12:49:07 +0400

    0.1.3.2.2 version - добавил docker-compose.yaml & Dockerfile & .env-postgresql

--- sweater/__init__.py
+++ sweater/__init__.py
@@ -4,13 +4,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
-from sweater import models, routes
-
 
 app = Flask(__name__)
 auth = Blueprint('auth', __name__)
 app.secret_key = '12QwrT!'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:admin1234@localhost:5432/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:admin1234@localhost:5432/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -19,3 +17,6 @@ manager.login_view = 'auth.login'
 bootstrap = Bootstrap(app)
 
 db.create_all()
+
+
+from sweater import models, routes
