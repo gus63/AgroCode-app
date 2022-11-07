d538ef8e986cb71817f062660a21ec6db521c826
Slawa163 <tvm91@icloud.com>
2022-11-07 23:31:14 +0400

    0.1.3.3 version

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ from wsgiref.simple_server import WSGIServer
 def runserver():
     if __name__ == '__main__':
         app = create_app()
+        app.app_context().push()
         app.run(debug=True, host='0.0.0.0', port=80)
     #   http_server = WSGIServer(('', 5000), app)
     #   http_server.serve_forever()
