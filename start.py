a5fcf277427624a595ed43659d61f81acf7a26a9
Slawa163 <tvm91@icloud.com>
2022-11-07 23:43:06 +0400

    0.1.3.3 version

--- start.py
+++ start.py
@@ -6,6 +6,6 @@ def runserver():
     if __name__ == '__main__':
         with app.app_context():
             db.create_all()
-        app.run(debug=True, host='0.0.0.0', port=80)
+        app.run(debug=True, host='127.0.0.1', port=5000)
     #   http_server = WSGIServer(('', 5000), app)
     #   http_server.serve_forever()
