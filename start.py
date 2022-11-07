299458637cdf3f3f64c7c9d0cf661c309f4b00a9
Slawa163 <tvm91@icloud.com>
2022-11-07 21:52:24 +0400

    0.1.3.3 version

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
-from wsgiref.simple_server import WSGIServer
 from sweater import app
+from wsgiref.simple_server import WSGIServer
 
 
 def runserver():
     if __name__ == '__main__':
-        app = create_app()
-        app.run(debug=True, host='127.0.0.1', port=5000)
+    #   app = create_app()
+        app.run(debug=False, host='127.0.0.1', port=5000)
         http_server = WSGIServer(('', 5000), app)
         http_server.serve_forever()
--- sweater/__init__.py
+++ sweater/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask, Blueprint
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+import config
 
 
 app = Flask(__name__)
+app.config.from_object(config)
 auth = Blueprint('auth', __name__)
 app.secret_key = '12QwrT!'
+# для portainer BD-->
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:admin1234@postgres:5432/postgres"
+# для локальной BD -->
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:admin1234@localhost:5432/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
 manager = LoginManager(app)
