6577f1b9a541e18814d18f5f24a1784e7484c095
Slawa163 <tvm91@icloud.com>
2022-11-07 20:56:49 +0400

    0.1.3.3 version

--- start.py
+++ start.py
@@ -5,6 +5,6 @@ from sweater import app
 def runserver():
     if __name__ == '__main__':
         app = create_app()
-        # app.run(debug=True, host='0.0.0.0', port=5000)
+        app.run(debug=True, host='127.0.0.1', port=5000)
         http_server = WSGIServer(('', 5000), app)
         http_server.serve_forever()
