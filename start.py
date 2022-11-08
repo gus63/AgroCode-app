6252b79fd87fafecc5c0dfe5467828598656db6e
Slawa163 <tvm91@icloud.com>
2022-11-08 21:33:20 +0400

    version

--- start.py
+++ start.py
@@ -2,10 +2,11 @@ from sweater import app, db
 from wsgiref.simple_server import WSGIServer
 
 
-#def runserver():
- #   if __name__ == '__main__':
+def runserver():
+    if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0')
   #      with app.app_context():
   #          db.create_all()
-   #     app.run(debug=True, host='0.0.0.0')
+
     #   http_server = WSGIServer(('', 5000), app)
     #   http_server.serve_forever()
