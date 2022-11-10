934e6e20534253e9ef91d0ddb9996301e84dddaa
Slawa163 <tvm91@icloud.com>
2022-11-10 15:50:28 +0400

    version

--- sweater/__init__.py
+++ sweater/__init__.py
@@ -6,14 +6,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 auth = Blueprint('auth', __name__)
-app.secret_key = '12QwrT!'
 # для portainer BD-->
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:admin1234@postgres:5432/postgres"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+app.secret_key = '12QwrT!'
 # для локальной BD -->
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:admin1234@localhost:5432/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 manager = LoginManager(app)
 manager.login_view = 'auth.login'
 bootstrap = Bootstrap(app)
