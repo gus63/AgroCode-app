c0726455220b48c907fec3e2a7f7e3e5575f9889
Slawa163 <tvm91@icloud.com>
2022-11-13 12:50:10 +0400

    version

--- sweater/__init__.py
+++ sweater/__init__.py
@@ -10,17 +10,14 @@ app.secret_key = '12QwrT!'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:admin1234@postgres:5432/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
-
-#from sweater.models import Role, User
+migrate = Migrate(app, db)
+manager = LoginManager(app)
+manager.login_view = 'auth.login'
+bootstrap = Bootstrap(app)
+from sweater import models, routes
 
 #with app.app_context():
 #    db.drop_all()
 #    db.create_all()
 #    db.session.commit()
 
-from sweater import models, routes
-
-migrate = Migrate(app, db)
-manager = LoginManager(app)
-manager.login_view = 'auth.login'
-bootstrap = Bootstrap(app)
